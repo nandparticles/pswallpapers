a96bc4bbf4ffea53b029a929b3db832e22bc9c63
Nandparticles <156614184+nandparticles@users.noreply.github.com>
2024-10-29 01:12:49 +0500

    Update script.js
    add r/darkwallpaper

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 async function fetchImages() {
-    const subreddits = ['Amoledbackgrounds', 'wallpapers'];
+    const subreddits = ['Amoledbackgrounds', 'wallpapers', 'darkwallpaper'];
     const imageUrls = [];
 
     for (const subreddit of subreddits) {
-        const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?limit=150`);
+        const response = await fetch(`https://www.reddit.com/r/${subreddit}/top.json?limit=10`);
         const data = await response.json();
         const posts = data.data.children;
 
